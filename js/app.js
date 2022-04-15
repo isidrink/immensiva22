b38c0a2df1c62c001099457781332bda51675f5b
Isidro Escoda <isidrink@gmail.com>
2022-04-15 10:11:23 +0200

    models AR

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/deploy.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/deployWebAR.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
